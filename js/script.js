2ae98a4cd10dd8d6e839ea2201db23cc47f95e25
Joaquin Liniers <liniersjoaquin@gmail.com>
2026-04-28 19:49:00 -0300

    Solucion errores

--- js/script.js
+++ js/script.js
@@ -8,22 +8,3 @@ function toggleTarjeta(elementoActivo) {
     elementoActivo.classList.toggle('abierta');
 }
 
-function copiarMail() {
-    const mail = "lucariver11@gmail.com";
-    navigator.clipboard.writeText(mail).then(() => {
-        const tooltip = document.getElementById("tooltipMail");
-        const icono = document.getElementById("iconoMail");
-        
-        tooltip.classList.add("mostrar-tooltip");
-        icono.classList.replace("fa-copy", "fa-check");
-        icono.style.color = "#4CAF50";
-
-        setTimeout(() => {
-            tooltip.classList.remove("mostrar-tooltip");
-            icono.classList.replace("fa-check", "fa-copy");
-            icono.style.color = "";
-        }, 2000);
-    }).catch(err => {
-        console.error('Error al copiar: ', err);
-    });
-}
